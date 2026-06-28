a71ac3a89669d42b01f977d09a4afb43d5cbef87
anicsonrico <anicsonrico@gmail.com>
2026-06-28 15:05:20 +0200

    unit tests TDD fo withdrawal useCases written

---
+++ src/main/java/bank/domain/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package bank.domain;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
